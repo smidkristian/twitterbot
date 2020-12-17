64c59206f214ab64dc5653f46c6ae520662191b4
smidkristian <smid.kristian@gmail.com>
2020-12-18 00:52:18 +0100

    first tweet

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Yo, I am a Twitter bot!");
+var fs = require('fs'),
+	path = require('path'),
+	Twit = require('twit'),
+	config = require(path.join(__dirname, 'config.js'));
+
+var T = new Twit(config);
+
+T.post('statuses/update', { status: 'My first tweet!' }, function(err, data, response) {
+	console.log(data)
+});
